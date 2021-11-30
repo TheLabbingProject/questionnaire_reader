4599fac118284ba4cb7d3dbdf41b6461a86b174c
GalBenZvi <hershkovitz1@mail.tau.ac.il>
2021-11-30 13:23:56 +0200

    fixed shs estimation

--- questionnaire_reader/shs.py
+++ questionnaire_reader/shs.py
@@ -1,11 +1,13 @@
-import pandas as pd
-
 from enum import Enum
 
-NORMAL_SCORING = ["SHS Q1","SHS Q2","SHS Q3"]
+import pandas as pd
+
+NORMAL_SCORING = ["SHS Q1", "SHS Q2", "SHS Q3"]
 REVERSED_SCORING = ["SHS Q4"]
-def calculate_bfi(data: pd.Series) -> pd.Series:
+
+
+def calculate_shs(data: pd.Series) -> pd.Series:
     shs_data = data[NORMAL_SCORING]
     reverse = 8 - data[REVERSED_SCORING]
     shs_data[REVERSED_SCORING] = reverse
-    return shs_data.mean()
+    return shs_data.mean(axis=1)
